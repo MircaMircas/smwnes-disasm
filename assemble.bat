fb184ddfe0b892a3e0a8ccb04913a23fa8970e22
MircaMircas <mikosmikus@gmail.com>
2026-01-18 11:39:08 +0100

    Add datetime backup system (Windows)

--- assemble.bat
+++ assemble.bat
@@ -1,2 +1,7 @@
-asm6 smw.asm smw.nes
+set datetime=%date%_%time%
+set datetime=%datetime: =_%
+set datetime=%datetime::=%
+set datetime=%datetime:/=_%
+set datetime=%datetime:.=_%
+asm6 smw.asm smw_%datetime%.nes
 pause
